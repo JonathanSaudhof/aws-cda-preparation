9834bd1160608d2473915f610220f9c1c1c1e128
Jonathan Saudhof <jonathan@jonathansaudhof.de>
2020-08-23 17:01:40 +0200

    lesson 2: deploying the app on eb

---
+++ 1_docker-full-stack/frontend/build/precache-manifest.fa9312d5181249ea7602f0bc1b2a19f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c36fe1795f4075c7cf5b6adfb609206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12c1f6b747d6bfcbc031",
+    "url": "/static/css/main.258130a3.chunk.css"
+  },
+  {
+    "revision": "420417b3748c01ffee11",
+    "url": "/static/js/2.66c790d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66c790d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c1f6b747d6bfcbc031",
+    "url": "/static/js/main.5323fbda.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
